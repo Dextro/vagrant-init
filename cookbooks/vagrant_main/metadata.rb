5afef35d4ff278e23fec299183279d59cf9661b6
Bert Pattyn <github@dextrose.be>
2012-11-02 23:48:21 +0100

    cleanup documentation of vagrant_main

---
+++ cookbooks/vagrant_main/metadata.rb
@@ -0,0 +1,10 @@
+maintainer       "Bert Pattyn"
+maintainer_email "github@dextrose.be"
+license          "MIT"
+description      "Main cookbook to initiate the recipes"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+%w{ apt vim git curl }.each do |cookbook|
+  depends cookbook
+end
