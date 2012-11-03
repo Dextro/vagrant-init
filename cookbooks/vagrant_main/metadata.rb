50061828fcc7b4dbab90462d4939e01db171a97a
Bert Pattyn <github@dextrose.be>
2012-11-03 21:44:51 +0100

    make vagrant_main cookbook dependable of the tmux and ack cookbook

--- cookbooks/vagrant_main/metadata.rb
+++ cookbooks/vagrant_main/metadata.rb
@@ -5,6 +5,6 @@ description      "Main cookbook to initiate the recipes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-%w{ apt vim git curl }.each do |cookbook|
+%w{ apt vim git curl ack tmux}.each do |cookbook|
   depends cookbook
 end
